b248034a01b157a6723da2763dd5142f2f66b435
andrei1gabriel <andrei.gabriel.manoliu@stud.ubbcluj.ro>
2025-03-29 19:33:49 +0200

    ok

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: E:/CLion C++ Proiecte/proiect_sda
+# Install script for directory: C:/Users/gabim/Documents/GitHub/sda_proiect
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -52,6 +52,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "E:/CLion C++ Proiecte/proiect_sda/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/gabim/Documents/GitHub/sda_proiect/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
